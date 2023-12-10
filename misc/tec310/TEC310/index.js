1223acd662dab8ed70ac6f3c9f3bc1e004f3eeeb
alikilg <153240793+alikilg@users.noreply.github.com>
2023-12-10 02:52:39 +0200

    Update index.js

--- misc/tec310/TEC310/index.js
+++ misc/tec310/TEC310/index.js
@@ -1,3 +1,44 @@
+//intro1
+$(document).ready(function(){
+  $("#n0a").hover(function(){
+    $("#intro1").show(200);
+    $("#n0a").hide(0);
+    $("#n0b").show(0);
+  });
+});
+
+//intro2
+$(document).ready(function(){
+  $("#intro1").click(function(){
+    $("#intro1").hide(55);
+    $("#intro2").show(35);
+  });
+});
+
+//intro3
+$(document).ready(function(){
+  $("#intro2").click(function(){
+    $("#intro2").hide(55);
+    $("#intro3").show(35);
+  });
+});
+
+//intro4
+$(document).ready(function(){
+  $("#intro3").click(function(){
+    $("#intro3").hide(55);
+    $("#intro4").show(35);
+  });
+});
+
+//intro5
+$(document).ready(function(){
+  $("#intro4").click(function(){
+    $("#intro4").hide(55);
+    $("#intro5").show(35);
+  });
+});
+
 //buttons
 $(document).ready(function(){
   $("#intro5").click(function(){
